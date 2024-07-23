4dcf366a7944c80a28e5255f168f06d212f00f36
shuminchang <magic2313@gmail.com>
2024-07-24 00:13:51 +0800

    add HomePage, WicketApplication

---
+++ src/main/java/com/giffing/wicket/spring/boot/starter/web/pages/HomePage.java
@@ -0,0 +1,8 @@
+package com.giffing.wicket.spring.boot.starter.web.pages;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+
+@WicketHomePage
+public class HomePage extends WebPage {
+}
